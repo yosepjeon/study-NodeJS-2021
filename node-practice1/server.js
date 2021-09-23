6378bcb07bb13d4da107f0d7a0d0df3e9cdba160
yosepjeon <enekelx1@naver.com>
2021-09-23 17:03:28 +0900

    node 개괄 학습 complete

--- node-practice1/server.js
+++ node-practice1/server.js
@@ -1,1 +1,45 @@
 const express = require('express')
+const app = express()
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  password : "enekelx1",
+  connectionLimit: 5,
+  database: "yos"
+});
+
+const server = app.listen(3000, () => {
+  console.log("Start Server : localhost: 3000")
+})
+
+app.set("views", __dirname + "/views")
+app.set("view engine", "ejs")
+app.engine("html", require('ejs').renderFile)
+
+app.get('/', function (req, res) {
+  res.render("index.html")
+})
+
+app.get('/about', function (req, res) {
+  res.render("about.html")
+})
+
+app.get('/products', function (req, res) {
+  pool.getConnection()
+    .then(conn => {
+    
+      conn.query("SELECT * FROM yos_product")
+        .then(result => { // res: { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          res.send(result)
+          conn.release(); // release to pool
+        })
+        .catch(err => {
+          conn.release(); // release to pool
+        })
+        
+    }).catch(err => {
+      //not connected
+    });
+})
